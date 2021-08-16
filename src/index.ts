5e1235f89f86b3f898d8b3557d6d6e9ab32a3797
ebolax <ebolax@gmail.com>
2021-08-16 15:04:26 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ import config from "./config";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-const game: Phaser.Game = new Phaser.Game(config);
-console.log(game);
+new Phaser.Game(config);
