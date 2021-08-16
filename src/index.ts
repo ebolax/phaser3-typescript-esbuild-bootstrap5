d94546f16f6f37b68a5ce0d6e7f12b130fb15381
ebolax <ebolax@gmail.com>
2021-08-16 15:02:13 +0300

    typescript-eslint

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const game: Phaser.Game = new Phaser.Game(config);
+console.log(game);
